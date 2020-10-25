ef6cd0dc5ae67c0b73bde8c66c7781b64e83667c
user47518079 <67531850+user47518079@users.noreply.github.com>
2020-10-26 08:46:21 +1100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1oT4GuBfKwfhUUErKjc7xHrjMA9pVcU4wjllMjEEfYj4/edit#gid=745832354';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1dF5RaTmgWiVBOzd2Ar9SbDwdx-1Cgo686OSuyNVmCOs/edit#gid=0';
